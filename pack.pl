f8cce8405c474b64e569a58634bf5dfd0b2c9ccc
Phil Hargett <phil@haphazardhouse.net>
2023-10-28 09:33:42 -0700

    bump version

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(aop).
 title('Opinonated aspect-oriented programming for prolog').
-version('0.0.8').
+version('0.0.9').
 author('Phil Hargett','phil@haphazardhouse.net').
 maintainer('Phil Hargett', 'phil@haphazardhouse.net').
 packager('Phil Hargett', 'phil@haphazardhouse.net').
